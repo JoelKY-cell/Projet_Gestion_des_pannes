352cccbf49773e5f85abd108d82d6fd82b93a8f1
JoelKY-cell <joelkyras3@gmail.com>
2025-06-03 22:06:24 +0000

    Update urls.py

--- monprojet/urls.py
+++ monprojet/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', auth_views.LoginView.as_view(), name='login'),
     path('monapplication/', include('monapplication.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
